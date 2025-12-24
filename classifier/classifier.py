2248f2298d49b887e650bd44b010e82913e32e0a
Dev Mukherjee <devmukherjeeindia@gmail.com>
2025-12-24 22:07:59 +0530

    classifier verified for few example classification.

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -5,8 +5,15 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sentence_transformers import SentenceTransformer
 import os
 
+
 DATASET_PATH = os.path.join("data", "security_nlp_cli_10000.csv")
+#DATASET_PATH = os.path.join("data", "security_nlp_cli_10000.csv")
+
+#DATASET_PATH = os.path.join("data", "security_nlp_cli_upgraded.csv")
+#DATASET_PATH = os.path.join("data", "security_nlp_cli_upgraded_curl.csv")
+#DATASET_PATH = os.path.join("data", "security_nlp_cli_upgraded_wocurl.csv")
 SIMILARITY_THRESHOLD = 0.45   # robust default
+
 
 class EmbeddingClassifier:
     def __init__(self):
